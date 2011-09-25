171c79a450ecf0aa5cfa04350e994ff4c4050d61
Hart <goatman.py@gmail.com>
2011-09-25 23:44:52 +0800

    refactored opencv-gtk.py demo

--- examples-ctypes/opencv-gtk.py
+++ examples-ctypes/opencv-gtk.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# may5th 2011, test ported to ctypes opencv
+# updated sept 25, 2011
 import os,sys, time, ctypes
 
 if '..' not in sys.path: sys.path.append( '..' )
@@ -13,10 +13,6 @@ assert gui
 gtk = rpythonic.module( 'gtk' )
 assert gtk
 gtk.init()
-
-#pix = gtk.gdk_pixbuf_new( gtk.GDK_COLORSPACE_RGB, True, 8, 640, 480)
-#img = gtk.Image()
-#assert 0
 
 _colorspaces = '''
 CV_BGR2GRAY
@@ -58,207 +54,123 @@ class LayerConfig(object):
 		self.split_red = self.split_green = self.split_blue = False
 		for fx in FXtypes: setattr(self, fx, False )
 
-BG_RED = 0.098
-BG_GREEN = 0.098
-BG_BLUE = 0.099
-BG_ALPHA = 0.0
-BG_COLOR = (BG_RED,BG_GREEN,BG_BLUE,BG_ALPHA)
+	def widget( self, notebook ):
+		cspace = ColorSpacesByValue[ self.colorspace ]
+		tag = cspace.split('2')[-1]
+		page = gtk.HBox()
+		h = gtk.HBox()
+		b = gtk.CheckButton()
+		#TODO b.connect('toggled', lambda b,lay: setattr(lay,'active',bool(b.get_active())), layer)
+		b.set_active( bool(self.active) )
+		h.pack_start( b, expand=False )
+		h.pack_start( gtk.Label(tag) )
+		notebook.append_page( page, h )
 
-def transparent_window( win, color=BG_COLOR, decorate=False ):
-	win.set_decorated( decorate )
-	# Tell GTK+ that we want to draw the windows background ourself.
-	# If we don't do this then GTK+ will clear the window to the
-	# opaque theme default color, which isn't what we want.
-	win.set_app_paintable(True)
-	make_transparent( win, color )
+		h.show_all()		# required - Gtk bug
+
+		col1, col2 = gtk.VBox(), gtk.VBox()
+		page.pack_start( col1, expand=False )
+		page.pack_start( col2, expand=True )
+
+		fxgroups = {}
+		for name in FXtypes:
+			bx = gtk.VBox()
+			fxgroups[ name.split('FX')[-1] ] = bx
+			frame = gtk.Frame(); frame.add( bx )
+			val = getattr( self, name )
+			b = gtk.CheckButton( name )
+			b.set_active( bool(val) )
+			# TODO b.connect('toggled', lambda b,lay,nam: setattr(lay,nam,bool(b.get_active())), layer, name)
+			frame.set_label_widget( b )
+			col2.pack_end( frame )
+
+		_skip = ['colorspace','active', 'widget']+FXtypes
+		for name in dir(self):
+			if not name.startswith('_') and name not in _skip:
+				val = getattr( self, name )
+				bx = None
+				for fx in fxgroups:
+					if name.startswith(fx):
+						bx = fxgroups[fx]
+						break
+				if not bx:
+					adjust = gtk.Adjustment(
+						value=val, 
+						lower=0, upper=255, 
+					)
+					# TODO adjust.connect("value_changed", lambda a,lay,nam: setattr(lay,nam,int(a.value)), layer,name)
+					scale = gtk.HScale( adjust ); scale.set_value_pos(gtk.POS_RIGHT)
+					scale.set_digits(0)
+					frame = gtk.Frame( name )
+					frame.add( scale )
+					col2.pack_start( frame )
+				else:
+					if fx=='split':
+						b = gtk.CheckButton( name )
+						b.set_active( bool(getattr(self,name)) )
+						# TODO b.connect('toggled', lambda b,lay,nam: setattr(lay,nam,bool(b.get_active())), layer, name)
+						bx.pack_start( b )
+					else:
+						upper = 255
+						lower = 0
+						step = 1
+						if name.startswith('sobel'):
+							upper = 31; lower = 1; step=2
+						adjust = gtk.Adjustment(
+							value=val, 
+							lower=lower, upper=upper, 
+						)
+						# TODO adjust.connect("value_changed", lambda a,lay,nam: setattr(lay,nam,int(a.value)), layer,name)
+						scale = gtk.HScale( adjust ); scale.set_value_pos(gtk.GTK_POS_RIGHT)
+						scale.set_digits(0)
+						row = gtk.HBox()
+						row.pack_start( gtk.Label( name.split(fx)[-1].replace('_',' ') ), expand=False )
+						row.pack_start( scale )
+						bx.pack_start( row )
 
 
-#def expose_transparent(widget, event):
-def expose_transparent():
-	for widget in _TRANSPARENT_WIDGETS_:
-		#cr = widget.window.cairo_create()
-		cr = gtk.gdk_cairo_create( widget.window )
-		#r,g,b,a = widget._trans_color_hack
-		r,g,b,a = BG_COLOR
-		cr.set_source_rgba(r, g, b, a) # Transparent
-		# Draw the background
-		cr.set_operator( gtk.cairo_operator['CAIRO_OPERATOR_SOURCE'] )
-		cr.paint()
-	return False
+class App(object):
+	def exit(self, *args):
+		self.active = False
+		self.webcam.active = False
 
-_TRANSPARENT_WIDGETS_ = []
-def make_transparent(widget, color=BG_COLOR ):
-	_TRANSPARENT_WIDGETS_.append( widget )
-	#widget._trans_color_hack = color
-
-	# The X server sends us an expose event when the window becomes
-	# visible on screen. It means we need to draw the contents.  On a
-	# composited desktop expose is normally only sent when the window
-	# is put on the screen. On a non-composited desktop it can be
-	# sent whenever the window is uncovered by another.
-	#
-	# The screen-changed event means the display to which we are
-	# drawing changed. GTK+ supports migration of running
-	# applications between X servers, which might not support the
-	# same features, so we need to check each time.
-	widget.connect('expose-event', expose_transparent)
-	#win.connect('screen-changed', self.screen_changed)
-	# To check if the display supports alpha channels, get the colormap
-	screen = widget.get_screen()
-	colormap = screen.get_rgba_colormap()
-	# Now we have a colormap appropriate for the screen, use it
-	widget.set_colormap(colormap)
-	return False
-
-BUFFER_TYPE = (ctypes.c_ubyte * (640*480*3))
-
-class UI(object):
-	def __init__(self, cam ):
-		cam._ui = self
+	def __init__(self, webcam ):
+		self.webcam = webcam
 		self.active = True
-		layers = self.layers = cam.layers		# shared
-
-		## gtk ##
 		self.window = win = gtk.window_new()		# do not use gtk.Window()
-		#win.set_default_size( 900, 480 )
-		win.set_title( 'remix' )
-		win.connect('destroy', lambda *args: gtk.main_quit() )
-		#transparent_window( win, decorate=True )
+		win.set_title( 'OpenCV+GTK' )
+		win.connect('destroy', self.exit )
 		root = gtk.HBox(); root.set_border_width( 3 )
 		win.add( root )
 
-		raw = BUFFER_TYPE()
-		for x in range( 640*480*3 ): raw[x] = 64
-		self.pixbuffer = ctypes.pointer( raw )
-		cam._pixbuffer = self.pixbuffer
-		#pix = gtk.gdk_pixbuf_new( gtk.GDK_COLORSPACE_RGB, True, 8, 640, 480)
-		pix = gtk.gdk_pixbuf_new_from_data(
-			self.pixbuffer, 
-			gtk.GDK_COLORSPACE_RGB,
-			False,	# ALPHA
-			8,		# bits per sample
-			640,
-			480,
-			640*3,	# row-stride
-		)
-		self._gdk_pixbuf = pix
-		img = gtk.gtk_image_new_from_pixbuf( pix )	# returns GtkWidget, should return GtkImage
-		self.image = img
-		root.pack_start( img )
-		#####################################
-		#eb = gtk.EventBox()
-		#root.pack_start( eb, expand=False )
-		#self._drawing_area = da = gtk.DrawingArea()
-		#da.set_size_request( 640,480 )
-		#da.connect('realize', self.realize)
-		#eb.add( da )
-		#make_transparent(da)
+		root.pack_start( webcam.gtk_image )
 
-		##################
-		#eb = gtk.EventBox()
-		#root.pack_start( eb, expand=True )
-		#root.pack_start( eb, expand=True, fill=True, padding=0 )
-		split = gtk.VBox(); #eb.add( split )
+		split = gtk.VBox()
 		root.pack_start( split )
-		#########################make_transparent(split)
-
-		
 
 		header = gtk.HBox(); split.pack_start( header, expand=False )
-		b = gtk.Button('open blender')
-		#b.connect('clicked', lambda b: os.system('%s -P %s --blender &'%(PATH2BLENDER, sys.argv[0])) )
-		header.pack_start( b, expand=False )
-
-		b = gtk.ToggleButton('[-]'); b.set_active(True)
-		#b.connect('toggled', lambda b: win.set_decorated( b.get_active() ) )
-		header.pack_start( b, expand=False )
 		
-
-		#ex = gtk.Expander( 'settings' ); ex.set_expanded(False)
-		#split.pack_start( ex, expand=False )
-		#ex = gtk.Expander( 'adjust layers' ); ex.set_expanded(True)
-		#split.pack_start( ex, expand=True)
-
 		note = gtk.Notebook()
 		note.set_tab_pos( gtk.POS_RIGHT )
-
-		#ex.add( note )
 		split.pack_start( note )
-		for layer in layers:
-				cspace = ColorSpacesByValue[ layer.colorspace ]
-				tag = cspace.split('2')[-1]
-				page = gtk.HBox()
-				h = gtk.HBox()
-				b = gtk.CheckButton()
-				#TODO b.connect('toggled', lambda b,lay: setattr(lay,'active',bool(b.get_active())), layer)
-				h.pack_start( b, expand=False )
-				h.pack_start( gtk.Label(tag) )
-				note.append_page( page, h )
-				h.show_all()
-				b.set_active( bool(layer.active) )
 
-				col1, col2 = gtk.VBox(), gtk.VBox()
-				page.pack_start( col1, expand=False )
-				page.pack_start( col2, expand=True )
+		for layer in webcam.layers:
+			layer.widget( note )
 
-				fxgroups = {}
-				for name in FXtypes:
-					bx = gtk.VBox()
-					fxgroups[ name.split('FX')[-1] ] = bx
-					frame = gtk.Frame(); frame.add( bx )
-					val = getattr( layer, name )
-					b = gtk.CheckButton( name )
-					b.set_active( bool(val) )
-					# TODO b.connect('toggled', lambda b,lay,nam: setattr(lay,nam,bool(b.get_active())), layer, name)
-					frame.set_label_widget( b )
-					col2.pack_end( frame )
-
-				for name in dir(layer):
-					if not name.startswith('_') and name not in ['colorspace','active']+FXtypes:
-						val = getattr( layer, name )
-						bx = None
-						for fx in fxgroups:
-							if name.startswith(fx):
-								bx = fxgroups[fx]
-								break
-						if not bx:
-							adjust = gtk.Adjustment(
-								value=val, 
-								lower=0, upper=255, 
-							)
-							# TODO adjust.connect("value_changed", lambda a,lay,nam: setattr(lay,nam,int(a.value)), layer,name)
-							scale = gtk.HScale( adjust ); scale.set_value_pos(gtk.POS_RIGHT)
-							scale.set_digits(0)
-							frame = gtk.Frame( name )
-							frame.add( scale )
-							col2.pack_start( frame )
-						else:
-							if fx=='split':
-								b = gtk.CheckButton( name )
-								b.set_active( bool(getattr(layer,name)) )
-								# TODO b.connect('toggled', lambda b,lay,nam: setattr(lay,nam,bool(b.get_active())), layer, name)
-								bx.pack_start( b )
-							else:
-								upper = 255
-								lower = 0
-								step = 1
-								if name.startswith('sobel'):
-									upper = 31; lower = 1; step=2
-								adjust = gtk.Adjustment(
-									value=val, 
-									lower=lower, upper=upper, 
-								)
-								# TODO adjust.connect("value_changed", lambda a,lay,nam: setattr(lay,nam,int(a.value)), layer,name)
-								scale = gtk.HScale( adjust ); scale.set_value_pos(gtk.GTK_POS_RIGHT)
-								scale.set_digits(0)
-								row = gtk.HBox()
-								row.pack_start( gtk.Label( name.split(fx)[-1].replace('_',' ') ), expand=False )
-								row.pack_start( scale )
-								bx.pack_start( row )
 
 		win.show_all()
 
-class Camera(object):
+	def main(self):
+		while self.active:
+			if gtk.gtk_events_pending():
+				while gtk.gtk_events_pending():
+					gtk.gtk_main_iteration()
+
+			self.webcam.iterate()
+
+class WebCamera(object):
+	BUFFER_TYPE = (ctypes.c_ubyte * (640*480*3))
+
 	_default_spaces = [ 
 		cv.CV_BGR2RGB, 
 		cv.CV_BGR2HSV,
@@ -267,16 +179,33 @@ class Camera(object):
 	]
 
 	def __init__(self):
-		self.active = False
+		self.active = True
 		self.index = 0
 		self.cam = gui.CreateCameraCapture(self.index)
-		#self.win = gui.NamedWindow('webcam', 1)
 		self.resize( 640, 480 )
 
+		########### Layer Configs ############
 		self.layers = []
 		for colorspace in self._default_spaces:
 			self.layers.append( LayerConfig( colorspace ) )
 		self.layers[0].active = True
+
+
+		raw = self.BUFFER_TYPE()
+		for x in range( 640*480*3 ): raw[x] = 64
+		ptr = ctypes.pointer( raw )
+		pix = gtk.gdk_pixbuf_new_from_data(
+			ptr, 
+			gtk.GDK_COLORSPACE_RGB,
+			False,	# ALPHA
+			8,		# bits per sample
+			640,
+			480,
+			640*3,	# row-stride
+		)
+		img = gtk.gtk_image_new_from_pixbuf( pix )	# returns GtkWidget, should return GtkImage
+		self.gtk_image = img
+
 
 	def resize( self, x,y ):
 		self.width = x
@@ -294,19 +223,14 @@ class Camera(object):
 		self._B = cv.CreateImage((self.width,self.height), cv.IPL_DEPTH_8U, 1)
 		self._A = cv.CreateImage((self.width,self.height), cv.IPL_DEPTH_8U, 1)
 
-	def loop( self ):
-		self.active = True
-
+	def iterate( self ):
 		_rgb8 = self._rgb8
 		_rgb32 = self._rgb32
 		_gray8 = self._gray8
 		_gray32 = self._gray32
 
-		while self.active:
-			if gtk.gtk_events_pending():
-				while gtk.gtk_events_pending():
-					gtk.gtk_main_iteration()
-			print('sync cam')
+		if self.active:
+
 			_frame = self.cam.QueryFrame()	# IplImage from highgui lacks fancy methods
 
 			for layer in self.layers:
@@ -356,35 +280,25 @@ class Camera(object):
 						cv.CvtColor(a, _rgb8, cv.CV_GRAY2RGB)
 						a = _rgb8
 
-				#gui.ShowImage( 'webcam', a )
-				#gui.WaitKey(1)
-				s = ctypes.string_at( a.imageData, 640*480*3 )
-				#print( s )
-				#ptr = ctypes.cast( a.imageData, ctypes.POINTER( BUFFER_TYPE ) )
-				#ctypes.memmove( self._pixbuffer, ptr, 640*480*3 )
-				#for i in range(640*480*3): self._pixbuffer.contents[i]= ord(s[i])
+				self.update_preview_image( a.imageData )
 
-				raw = BUFFER_TYPE()
-				for i in range( 640*480*3 ): raw[i] = ord(s[i])
-				pixbuffer = ctypes.pointer( raw )
-				#pix = gtk.gdk_pixbuf_new( gtk.GDK_COLORSPACE_RGB, True, 8, 640, 480)
-				pix = gtk.gdk_pixbuf_new_from_data(
-					pixbuffer, 
-					gtk.GDK_COLORSPACE_RGB,
-					False,	# ALPHA
-					8,		# bits per sample
-					640,
-					480,
-					640*3,	# row-stride
-				)
-				#self._ui.image.set_from_pixbuf( pix )	# TODO fix me
-				gtk.image_set_from_pixbuf( self._ui.image, pix )
-				#pix.copy_area( 0, 0, 640, 480, self._ui._gdk_pixbuf, 0, 0 )
 
-		print('exit camera loop')
+	def update_preview_image(self, pointer ):
+		#ptr = ctypes.pointer( BUFFER_TYPE() )
+		#ctypes.memmove( ptr, pointer, 640*480*3 )	# not required
+		pix = gtk.gdk_pixbuf_new_from_data(
+			pointer, 
+			gtk.GDK_COLORSPACE_RGB,
+			False,	# ALPHA
+			8,		# bits per sample
+			640,
+			480,
+			640*3,	# row-stride
+		)
+		gtk.image_set_from_pixbuf( self.gtk_image, pix )
 
-c = Camera()
-ui = UI( c )
-c.loop()
-#gtk.main()
+
+c = WebCamera()
+app = App( c )
+app.main()
 
