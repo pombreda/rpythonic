6574f344984c0243b4f26b16a11aaa88179daef4
Hart <goatman.py@gmail.com>
2012-04-17 22:00:15 +0800

    added support for rpython syntax "assert isinstance(a,A)" to neorpython.py

--- rpythonic/neorpython.py
+++ rpythonic/neorpython.py
@@ -1,4 +1,4 @@
-# Neo-Rpython - April 15, 2012
+# Neo-Rpython - April 17, 2012
 # by Brett and The Blender Research Lab.
 # License: BSD
 
@@ -7,37 +7,29 @@ def make_rpython_compatible( graph, delete_class_properties=True, debug=True ):
 	modifies the flowgraph in place to make it strict-Rpython compatible
 	'''
 	if debug: print('=============== make rpython compatible ===============')
-
-	class_props = process_blocks( graph.iterblocks() )
-
-	blocks = [ link.target for link in graph.iterlinks() ]
-	process_blocks( blocks )
-
-	if delete_class_properties:
-		for cls in class_props:	# delete the properties
-			for name in class_props[ cls ]: delattr( cls, name )
-
-
-def process_blocks( blocks, debug=True ):
 	import pypy.objspace.flow.model
 
-	class_props = {}	# return dict of: class : [ prop names ]
+	#class_props = process_blocks( graph.iterblocks() )
+	#blocks = [ link.target for link in graph.iterlinks() ]
+	#process_blocks( blocks )
 
+	class_props = {}	# return dict of: class : [ prop names ]
+	blocks = list(graph.iterblocks())
 	for block in blocks:
 		cache = {}
 		insert = []
 		if debug: print( block )
 
-		if block.exits:
-			print( 'SUB BLOCKS', block.exits )
-			process_blocks( [link.target for link in block.exits] )
+		#if block.exits:
+		#	print( 'SUB BLOCKS', block.exits )
+		#	process_blocks( [link.target for link in block.exits] )
 
 		for op in block.operations:
 			if debug: print( op )
 
 			if op.opname in ('getitem','setitem') and isinstance( op.args[0], pypy.objspace.flow.model.Variable ):
 				instance_var = op.args[0]
-				cls = get_class_helper( block, instance_var )
+				cls = get_class_helper( graph, block, instance_var )
 				if not cls:
 					print("WARN, can't find class")
 					continue
@@ -68,7 +60,7 @@ def process_blocks( blocks, debug=True ):
 
 			elif op.opname == 'simple_call' and isinstance( op.args[0], pypy.objspace.flow.model.Variable ):
 				instance_var = op.args[0]
-				cls = get_class_helper( block, instance_var )
+				cls = get_class_helper( graph, block, instance_var )
 				if not cls: continue
 
 				func_name = '__call__'
@@ -96,7 +88,7 @@ def process_blocks( blocks, debug=True ):
 			elif op.opname in ('setattr', 'getattr'):
 				instance_var = op.args[0]; name_const = op.args[1]
 				name = name_const.value	# <class 'pypy.objspace.flow.model.Constant'>
-				cls = get_class_helper( block, instance_var )
+				cls = get_class_helper( graph, block, instance_var )
 
 				if cls and hasattr(cls, name) and type(getattr(cls,name)) is property:
 					prop = getattr(cls,name)
@@ -135,30 +127,68 @@ def process_blocks( blocks, debug=True ):
 					op.opname = 'simple_call'
 
 		## insert the get-method-op before the simple_call ##
+		modified = bool( insert )
 		while insert:
 			op, getop = insert.pop()
 			index = block.operations.index( op )
 			block.operations.insert( index, getop )
-		if debug:
+		if modified and debug:
 			print('------- modified ops -------')
 			for op in block.operations: print(op)
 			print('-'*80)
 
+
+
+	if delete_class_properties:
+		for cls in class_props:	# delete the properties
+			for name in class_props[ cls ]: delattr( cls, name )
+
 	return class_props		# returns class props to be removed before annotation
 
 
-def get_class_helper( block, var ):
+def get_class_helper( graph, block, var ):
 	'''
 	if the creation of the instance is outside of the block this fails.
 	TODO traverse ancestor blocks until class is found.
 	'''
 	import pypy.objspace.flow.model
 
-	for op in block.operations:
-		if op.result is var:
-			if op.opname == 'simple_call' and isinstance( op.args[0], pypy.objspace.flow.model.Constant ):
-				cls = op.args[0].value
-				return cls
+	#if block.exits:
+	#	print( 'SUB BLOCKS', block.exits )
+	#	blocks = [link.target for link in block.exits]
+	#	for b in blocks:
+	#		cls = get_class_helper( b, var )
+	#		if cls: return cls
+	if var in block.inputargs:		# if var is in the blocks input args, we need to check the other blocks
+		for b in graph.iterblocks():
+			if b is block: continue
+			check = False
+			for link in b.exits:
+				if link.target is block: check = link; break
+			if not check: continue
+			var = link.args[ block.inputargs.index(var) ]
+
+			for op in b.operations:
+				## "assert isinstance(a, A)" the hijacked type decl of Rpython,
+				## this always appears in an outside block from the one we are checking ##
+				if op.opname == 'simple_call' and isinstance( op.args[0], pypy.objspace.flow.model.Constant ) and len(op.args)==3:
+					a,b,c = op.args
+					if a.value is isinstance and b is var:
+						return c.value
+				elif op.result is var:
+					if op.opname == 'simple_call' and isinstance( op.args[0], pypy.objspace.flow.model.Constant ):
+						cls = op.args[0].value
+						if type(cls) is type:	# how to ensure its a class?
+							return cls
+
+	else:
+		for op in block.operations:
+			if op.result is var:
+				## check if the instance is created inside this block ##
+				if op.opname == 'simple_call' and isinstance( op.args[0], pypy.objspace.flow.model.Constant ):
+					cls = op.args[0].value
+					if type(cls) is type:	# how to ensure its a class?
+						return cls
 
 
 ############################## TESTING ######################################
@@ -183,6 +213,7 @@ if __name__ == '__main__':
 
 	def func(arg):
 		a = A()
+		assert isinstance( a, A )
 		a.myattr = 'foo'
 		s = a.myattr
 		a.myattr = s + 'bar'
