7e7364a5701fc2006a4a98b3f1f88055d7c07024
brett hartshorn <brett@insynchq.com>
2012-06-30 21:26:33 +0800

    threaded the inotify helloworld example

--- examples/inotify-helloworld.py
+++ examples/inotify-helloworld.py
@@ -17,32 +17,46 @@ size = ctypes.sizeof( inotify.inotify_event.CSTRUCT )
 buff = (ctypes.c_char * size)()
 ptr = ctypes.cast( buff, ctypes.POINTER(ctypes.c_void_p) )
 
+ACTIVE = True
+def loop( fd ):
+	while ACTIVE:
+		read, write, errors = select.select( [fd], [], [] )	# releases the GIL
+		if read:
+			unistd.read( fd, ptr, size )	# also releases the GIL
+			#print(''.join(buff))
+			event = ctypes.cast( ptr, ctypes.POINTER(inotify.inotify_event.CSTRUCT) ).contents
+			print(event)
+			#print('wd',event.wd)
+			#print('mask',event.mask)
+			#print('cookie',event.cookie)
+			#print('len', event.C_len)
+
+			mask = event.mask
+			if mask & inotify.ACCESS: print( 'access file' )
+			if mask & inotify.ATTRIB: print('attribs changed')
+			if mask & inotify.CLOSE_WRITE: print('close write')
+			if mask & inotify.CLOSE_NOWRITE: print('closed (no write)')
+			if mask & inotify.CREATE: print('create')
+			if mask & inotify.DELETE: print('something in directory removed')
+			if mask & inotify.DELETE_SELF: print('removed')
+			if mask & inotify.MODIFY: print('modified')
+
+			if mask & inotify.MOVE_SELF: print('moved')
+			if mask & inotify.MOVED_FROM: print('file moved out of watched dir')
+			if mask & inotify.MOVED_TO: print('moved into watched dir')
+			if mask & inotify.OPEN: print('open')
+
+	print( 'thread exit' )
+
+import threading
+
+threading._start_new_thread( loop, (fd,) )
+
 while True:
-	read, write, errors = select.select( [fd], [], [] )
-	if read:
-		unistd.read( fd, ptr, size )
-		#print(''.join(buff))
-		event = ctypes.cast( ptr, ctypes.POINTER(inotify.inotify_event.CSTRUCT) ).contents
-		print(event)
-		#print('wd',event.wd)
-		#print('mask',event.mask)
-		#print('cookie',event.cookie)
-		#print('len', event.C_len)
-
-		mask = event.mask
-		if mask & inotify.ACCESS: print( 'access file' )
-		if mask & inotify.ATTRIB: print('attribs changed')
-		if mask & inotify.CLOSE_WRITE: print('close write')
-		if mask & inotify.CLOSE_NOWRITE: print('closed (no write)')
-		if mask & inotify.CREATE: print('create')
-		if mask & inotify.DELETE: print('something in directory removed')
-		if mask & inotify.DELETE_SELF: print('removed')
-		if mask & inotify.MODIFY: print('modified')
-
-		if mask & inotify.MOVE_SELF: print('moved')
-		if mask & inotify.MOVED_FROM: print('file moved out of watched dir')
-		if mask & inotify.MOVED_TO: print('moved into watched dir')
-		if mask & inotify.OPEN: print('open')
-
-
+	print('tick')
+	try:
+		time.sleep(1)
+	except:
+		ACTIVE = False
+		break
 
