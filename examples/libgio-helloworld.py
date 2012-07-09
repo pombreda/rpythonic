a44c54d1763186ae5bfbc65ee996163da0307a31
brett hartshorn <brett@insynchq.com>
2012-07-09 12:39:15 +0800

    better helper Variant for glib

--- examples/libgio-helloworld.py
+++ examples/libgio-helloworld.py
@@ -14,32 +14,64 @@ print('info-icon', info.get_icon() )
 print('info-content-type', info.get_content_type() )
 
 ############## test Glib ################
-def Variant( *args ):
-	'''
-	can call this with multiple or single arguments,
-	if a single argument and its wrapped in a tuple,
-	then wrap Variant in a tuple. (required for remote method calls)
-	'''
-	if len(args) == 1:
-		a = args[0]
-		wrap = False
-		if isinstance(a, tuple):
-			wrap = True
-			a = a[0]
-		if isinstance(a, basestring):
-			var = glib.variant_new_string( a )
-		if wrap:
-			array = ( ctypes.POINTER(glib.GVariant.CSTRUCT)*1 )( var.POINTER )
-			var = glib.variant_new_tuple( array, 1 )
+def _python_simple_type_to_gvariant( arg ):
+		'''
+		TODO other int types, byte and bytearray
+		'''
+		if isinstance(arg, basestring):
+			var = glib.variant_new_string( arg )
+		elif isinstance(arg, float):
+			var = glib.g_variant_new_double( arg )
+		elif isinstance(arg, int):
+			var = glib.g_variant_new_int32( arg )
+		elif isinstance(arg, bool):
+			var = glib.g_variant_new_boolean( arg )
+
+		elif isinstance(arg, list):	# assume array (RECURSIVE)
+			array = ( ctypes.POINTER(glib.GVariant.CSTRUCT)*len(arg) )()
+			for i,a in enumerate(arg): array[ i ] = _python_simple_type_to_gvariant( a ).POINTER
+			return glib.variant_new_array( None, array, len(arg) )
+
+		else:
+			raise NotImplementedError
+
 		return var
 
-	elif len(args) > 1:
-		gargs = ( ctypes.POINTER(glib.GVariant.CSTRUCT)*len(args) )()
-		if type(args[0]) == str:
-			for i,a in enumerate(args):
-				gargs[i] = glib.variant_new_string( a ).POINTER
+def Variant( *args, **kw ):
+	'''
+	can call this with multiple or single arguments,
+	Single Argument:
+		. if argument is a tuple, then wrap Variant in a tuple. (required for remote method calls)
+		. if argument is a tuple, and container a sub-tuple, wrap return in tuple (required for remote properties "Set")
+	'''
+	if len(args) == 1:
+		sub = args[0]
+		array = None
+		if isinstance(sub, tuple) or 'wrap_tuple' in kw:
+			if isinstance(sub, tuple): _len = len(sub)
+			else: _len = 1; sub = [sub]
+			array = ( ctypes.POINTER(glib.GVariant.CSTRUCT)*_len )()
+		if array:
+			for i,arg in enumerate(sub): array[ i ] = _python_simple_type_to_gvariant( arg ).POINTER
+			return glib.variant_new_tuple( array, len(sub) )
+		else:
+			return _python_simple_type_to_gvariant( sub )
 
-		return glib.variant_new_array( None, gargs, len(args) )
+
+	elif len(args) > 1:
+		## check if all the same - if not then wrap in tuple ##
+		_types = [type(a) for a in args]
+		is_array = _types.count(_types[0]) == len(_types)
+		if 'wrap_tuple' in kw: is_array = False	# force over-ride
+
+		array = ( ctypes.POINTER(glib.GVariant.CSTRUCT)*len(args) )()
+		for i,a in enumerate(args):
+				array[ i ] = _python_simple_type_to_gvariant( a ).POINTER
+		if is_array:
+			return glib.variant_new_array( None, array, len(args) )
+		else:
+			return glib.variant_new_tuple( array, len(args) )
+
 
 hello = 'hello'
 world = 'world'
@@ -98,7 +130,6 @@ for i in range( a.n_children() ):
 
 #########################################
 ##Creates a proxy for accessing interface_name on the remote object at object_path owned by name at connection and synchronously loads D-Bus properties unless the G_DBUS_PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES flag is used. ##
-## possible issue with Properties: 'org.freedesktop.DBus.Properties'
 proxy = gio.dbus_proxy_new_sync(
 	bus,		# GIOStream
 	gio.G_DBUS_PROXY_FLAGS_NONE,
@@ -110,7 +141,10 @@ proxy = gio.dbus_proxy_new_sync(
 	None	# ctypes.POINTER(ctypes.POINTER(_GError)))
 )
 error = ctypes.pointer(ctypes.c_void_p())
-arg = Variant( tuple(['ctypes test']) )
+
+arg = Variant( tuple(['ctypes test']) )					# these are the same
+arg = Variant( 'ctypes test', wrap_tuple=True )	# these are the same
+
 arg.ref_sink()
 res = proxy.call_sync(
 	'hello_world', 
@@ -122,6 +156,40 @@ res = proxy.call_sync(
 )
 error = ctypes.cast( error, ctypes.POINTER(glib.GError.CSTRUCT) )
 assert not error.contents.code
+
+
+################ TODO FIX ME : Dbus Props ################
+## dbus props are slower than direct method calls ##
+#http://dbus.freedesktop.org/doc/dbus-specification.html#standard-interfaces
+proxy = gio.dbus_proxy_new_sync(
+	bus,		# GIOStream
+	gio.G_DBUS_PROXY_FLAGS_NONE,
+	None,	# info : ctypes.POINTER(_GDBusInterfaceInfo)
+	'org.cinnamon.insync',	# name
+	'/org/cinnamon/insync',	# object path
+	'org.freedesktop.DBus.Properties',	# interface name
+	None,	# ctypes.POINTER(_GCancellable))
+	None	# ctypes.POINTER(ctypes.POINTER(_GError)))
+)
+error = ctypes.pointer(ctypes.c_void_p())
+
+arg = Variant(
+	'org.cinnamon.insync',	# object interface
+	'recent_changes',			# property name
+	['ctypes-prop1', 'ctypes-prop2', 'ctypes-prop3']
+)
+arg.ref_sink()
+res = proxy.call_sync(
+	'Set', 
+	arg, 
+	gio.G_DBUS_CALL_FLAGS_NO_AUTO_START,
+	500,
+	None,
+	error
+)
+error = ctypes.cast( error, ctypes.POINTER(glib.GError.CSTRUCT) )
+assert not error.contents.code
+
 
 print('ctypes gio test complete')
 
